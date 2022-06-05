490c7aabb8669fe6b812e8543321fd1dc7dd9c91
Christopher Geary <hello@crgeary.com>
2022-06-05 11:58:02 +0100

    Add client next application, and configure eslint

---
+++ apps/client/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Index: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Studio</title>
+            </Head>
+        </>
+    );
+};
+
+export default Index;
