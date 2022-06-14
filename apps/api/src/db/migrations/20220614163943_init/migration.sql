c590cf2df38e8d64ed4d2364107eec68baf67fa5
Christopher Geary <hello@crgeary.com>
2022-06-14 17:40:26 +0100

    Use UUID instead of CUID

--- apps/api/src/db/migrations/20220614163943_init/migration.sql
+++ apps/api/src/db/migrations/20220614163943_init/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "users" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
     "deleted_at" TIMESTAMP(3),
