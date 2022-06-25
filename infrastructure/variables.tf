08efc7323d5f99628f2ba4119d4605059117dffb
Christopher Geary <hello@crgeary.com>
2022-06-25 17:40:36 +0100

    Small refactor to terraform files

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,12 +2,10 @@ variable "cloudflare_email" {
   type        = string
   description = "Cloudflare email address"
   sensitive   = true
-
 }
 
 variable "cloudflare_api_token" {
   type        = string
   description = "Cloudflare API token"
   sensitive   = true
-
 }
