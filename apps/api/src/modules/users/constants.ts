1af658907f55a772b53262198877b13843cb9e9e
Christopher Geary <hello@crgeary.com>
2022-07-23 09:10:07 +0100

    Remove JWT constants

--- apps/api/src/modules/users/constants.ts
+++
@@ -1,5 +0,0 @@
-export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
-export const JWT_ACCESS_TOKEN_LIFETIME = "15m";
-
-export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
-export const JWT_REFRESH_TOKEN_LIFETIME = "2w";
