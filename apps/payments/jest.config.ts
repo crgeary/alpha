1f3b3db5c85016dc3b2276a2804bd7fc0811ccb0
Christopher Geary <hello@crgeary.com>
2022-07-30 13:34:00 +0100

    Configure TS/Jest etc for monorepo

--- apps/payments/jest.config.ts
+++ apps/payments/jest.config.ts
@@ -3,7 +3,7 @@ export default {
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: "coverage",
-    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
+    collectCoverageFrom: ["src/**/*.{js,ts}"],
     coverageProvider: "v8",
     testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
     setupFiles: ["dotenv/config"],
--- packages/logger/jest.config.ts
+++ packages/logger/jest.config.ts
@@ -3,8 +3,7 @@ export default {
     clearMocks: true,
     collectCoverage: true,
     coverageDirectory: "coverage",
-    collectCoverageFrom: ["src/**/*.{js,ts}"],
+    collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
     coverageProvider: "v8",
     testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-    setupFiles: ["dotenv/config"],
 };
