02582b70dd79dda25dd31f401dee1bae4c70b938
Christopher Geary <hello@crgeary.com>
2022-08-12 19:07:32 +0100

    Remove /api

--- apps/api/src/common/logger.ts
+++
@@ -1,6 +0,0 @@
-import { createLogger } from "@alpha/logger";
-
-export const { logger } = createLogger({
-    name: "api",
-    environment: process.env.NODE_ENV,
-});
--- apps/api/src/common/types/index.ts
+++
@@ -1,1 +0,0 @@
-export type { Module } from "./module.type";
